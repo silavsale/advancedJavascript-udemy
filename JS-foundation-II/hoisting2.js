b0f04ee1a8272815be2907e41ba76b4079b7bb3a
Sergiy Rudenko <silavsale@gmail.com>
2019-12-25 15:36:34 -0500

    added hoistind

---
+++ JS-foundation-II/hoisting2.js
@@ -0,0 +1,10 @@
+var favourFood = "grapes";
+
+var foodThoughts = function () {
+    console.log("Original favourite food: ", favourFood);
+
+    var favourFood = "sushi";
+    console.log("New favourite food:", favourFood);
+}
+
+foodThoughts();
--- README.md
+++ README.md
@@ -5,3 +5,4 @@
 
 1. Execution Context
 2. Lexical Environment
+3. Hoisting
